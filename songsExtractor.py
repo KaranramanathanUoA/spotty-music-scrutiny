63ceed944a3b161d394a503901c600a9bad1da86
KaranramanathanUoA <knel697@aucklanduni.ac.nz>
2019-12-26 17:00:55 +1300

    Refactored method names and variables for clarity

--- songsExtractor.py
+++ songsExtractor.py
@@ -2,6 +2,8 @@ import sys
 import spotipy
 import spotipy.util as util
 import credentialManager
+from datetime import datetime
+import json
 
 scope = 'user-library-read'
 # Secrets are retrieved from credentialManager.py, which is also added to the .gitignore file to ensure security
@@ -9,7 +11,7 @@ clientId = credentialManager.SPOTIPY_CLIENT_ID
 clientSecret = credentialManager.SPOTIPY_CLIENT_SECRET
 redirectURI = credentialManager.SPOTIPY_REDIRECT_URI
 
-def extract_saved_songs_info_of_user_from_spotify(clientId, clientSecret, redirectURI):
+def extract_saved_songs_info_from_user_library(clientId, clientSecret, redirectURI):
     # Throw error if user name is not provided in the command line arguments
     if len(sys.argv) > 1:
         userName = sys.argv[1]
@@ -24,21 +26,21 @@ def extract_saved_songs_info_of_user_from_spotify(clientId, clientSecret, redire
         sp = spotipy.Spotify(auth = token)
         # First run through only returns 20 songs by default, but it also retrieves total no of songs in library
         response = sp.current_user_saved_tracks()
-        results = response['items']
+        savedSongsInfo = response['items']
         
         # subsequently runs until it has read all songs in the library
-        while (len(results) < response["total"]):
-            response = sp.current_user_saved_tracks(offset = len(results))
-            results.extend(response['items'])
-        return results
+        while (len(savedSongsInfo) < response["total"]):
+            response = sp.current_user_saved_tracks(offset = len(savedSongsInfo))
+            savedSongsInfo.extend(response['items'])
+        return savedSongsInfo
     
     else:
         print ("Can't get token for {0}".format(userName))
         sys.exit()
 
-def extract_song_details_from_saved_song_info(results):
+def extract_song_details(savedSongsInfo):
     detailsOfAllSongs = []
-    for item in results:
+    for item in savedSongsInfo:
         songDetails = {}
         track = item['track']
         trackName = track['name']
@@ -47,23 +49,49 @@ def extract_song_details_from_saved_song_info(results):
         songDetails['Track Name'] = trackName
         songDetails['Artist'] = artistName
         songDetails['Artist Id'] = artistId
-        songDetails['genres'] = extract_genres_of_artist(artistId)
         detailsOfAllSongs.append(songDetails)
     return detailsOfAllSongs
 
-def extract_genres_of_artist(artistId):
+def extract_genre_of_every_song_in_user_library(savedSongsInfo, detailsOfAllSongs):
+    listOfArtistIds = extract_artist_ids(savedSongsInfo)
+    artistInfo = extract_genres_of_all_artists(listOfArtistIds)
+    matchGenreToArtist(detailsOfAllSongs, artistInfo)
+
+def extract_artist_ids(savedSongsInfo):
+    listOfArtistIds = set()
+    for item in savedSongsInfo:
+        track = item['track']
+        artistId = track['artists'][0]['id']
+        listOfArtistIds.add(artistId)
+    return listOfArtistIds
+
+def extract_genres_of_all_artists(listOfArtistIds):
+    artistInfo = {}
     userName = sys.argv[1]
     token = util.prompt_for_user_token(userName, scope, client_id = clientId, client_secret = clientSecret, redirect_uri= redirectURI)
     sp = spotipy.Spotify(auth = token)
-    artist = sp.artist(artistId)
-    genres = artist['genres']
-    return genres
+    for artistId in listOfArtistIds:
+        artist = sp.artist(artistId)
+        genres = artist['genres']
+        artistInfo[artistId] = genres
+    return artistInfo
+
+def matchGenreToArtist(detailsOfAllSongs, artistInfo):
+    for song in detailsOfAllSongs:
+        artistId = song['Artist Id']
+        genres = artistInfo[artistId]
+        song.update({"Genre": genres})
+
+def printJsonContentsToFile(detailsOfAllSongs):
+    pass
 
 def main():
-    result = extract_saved_songs_info_of_user_from_spotify(clientId, clientSecret, redirectURI)
-    #print (result)
-    detailsOfAllSongs = extract_song_details_from_saved_song_info(result)
-    print (detailsOfAllSongs)
+    startTime = datetime.now()
+    savedSongsInfo = extract_saved_songs_info_from_user_library(clientId, clientSecret, redirectURI)
+    detailsOfAllSongs = extract_song_details(savedSongsInfo)
+    extract_genre_of_every_song_in_user_library(savedSongsInfo, detailsOfAllSongs)
+    print (datetime.now() - startTime)
+    #print (detailsOfAllSongs)
 
 if __name__ == "__main__":
     main()   
